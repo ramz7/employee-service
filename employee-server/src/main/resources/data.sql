8389d512aae3d62a1a6e5e699454a6ebc1980970
ramz7 <ramminarayanan7@gmail.com>
2020-06-29 00:07:51 +0530

    sql added

---
+++ employee-server/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+--designation
+INSERT INTO `employee-system`.`designation` (`id`, `name`) VALUES ('1', 'Software Engineer');
+
+--department
+INSERT INTO `employee-system`.`department` (`id`, `name`) VALUES ('1', 'Engineering');
+
